b9b613d78cecac596e116af10f85ae3b97ba9446
soberdier <soberdier@gmail.com>
2013-08-14 17:24:57 -0400

    dog years

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -1,1 +1,10 @@
-// JavaScript Document
+// Dog Years
+
+var humanYears = 3; //declaring and defining sparky's age in human years
+
+var dogYears = 7; //declaring and defining 1 human year to dog years
+
+dogAge = humanYears * dogYears; // calculating sparky's dog year age (result)
+
+console.log(dogAge); // printing the calculation
+console.log("dog years old")
