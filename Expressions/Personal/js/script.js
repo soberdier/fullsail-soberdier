3c4c15ab04bb94e4e566785cf2750523ce8c3ad8
soberdier <soberdier@gmail.com>
2013-08-15 18:45:44 -0400

    Personal expressions started

---
+++ Expressions/Personal/js/script.js
@@ -0,0 +1,11 @@
+// Personal
+
+var gallons = prompt("How many gallons of gas does your tank hold")
+
+var price = 3.20
+
+var spent = prompt("How much do you usually spend at the pump?")
+
+var gas = spent/price
+
+alert("In most of the country you are usually getting about "+gas+" gallons of gas.")
