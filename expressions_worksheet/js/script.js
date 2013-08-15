fdac89d90294f13b7ffca3a26d8ec688db4bb355
soberdier <soberdier@gmail.com>
2013-08-15 16:45:18 -0400

    discounts

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -7,3 +7,17 @@ total = groceries[0] + groceries[1] + groceries[2] + groceries[3] + groceries[4]
 average = total / 5; 
 
 console.log("You have spent a total of $"+total+" on groceries over five weeks. That is an average of $"+average+" per week.") //printing the result.
+
+//Discounts
+
+var original = 24.99; //item original price
+
+var discount = original*.10; //discounted price
+
+var product = "xboxone"; //item purchased
+
+taxless = original - discount //discounted item before tax
+
+tax = taxless*.70 //the sales tax
+taxed = original - discount + tax //the final price for this item.
+console.log("Your " +product+ " was originally $"+original+",but after a " +10+ "% discount, it is now $"+taxless+" without tax, and $"+taxed+" with tax.")
