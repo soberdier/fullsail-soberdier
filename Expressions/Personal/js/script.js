2fc6cf38fd15eabbc98583dad5ab7306a58ea001
soberdier <soberdier@gmail.com>
2013-08-15 18:53:28 -0400

    personal complete

--- Expressions/Personal/js/script.js
+++ Expressions/Personal/js/script.js
@@ -1,11 +1,13 @@
 // Personal
 
-var gallons = prompt("How many gallons of gas does your tank hold")
+var gallons = prompt("How many gallons of gas does your tank hold"); //how many gallons your tank holds.
 
-var price = 3.20
+var price = 3.20; //on average the price of gas in Ohio.
 
-var spent = prompt("How much do you usually spend at the pump?")
+var spent = prompt("How much do you usually spend at the pump?"); //self explanitory
 
-var gas = spent/price
+var gas = spent/price; //calculating how much gas you would usually get in Ohio
 
-alert("In most of the country you are usually getting about "+gas+" gallons of gas.")
+alert("In Ohio you are usually getting about "+gas+" gallons of gas.") //informing the user how much gas they would get in Ohio
+
+alert("ProTip: If you hold the pump as loosely as possible you will get more gas for your buck!") //ProTip of the day!
