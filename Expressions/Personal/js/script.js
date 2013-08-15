31166dc23d391681c1e75fb68c1c0c8a105b7573
soberdier <soberdier@gmail.com>
2013-08-15 19:13:39 -0400

    Industry specific after annotations.

--- Expressions/Personal/js/script.js
+++ Expressions/Personal/js/script.js
@@ -1,15 +1,15 @@
 //Industry_Specific
 
-var work = prompt("How many hours does it take on average to build a 10 page website?");
+var work = prompt("How many hours does it take on average to build a 10 page website?"); //asking how long it takes in general to do a website.
 
-var clients = prompt("About how many clients do you take on in a year?");
+var clients = prompt("About how many clients do you take on in a year?"); //asking about how many clients you get a year.
 
-var pay = prompt("How much do you charge per hour?");
+var pay = prompt("How much do you charge per hour?"); //How much you get per hour.
 
-var job = pay*work;
+var job = pay*work; //calculating how much you get per client on average
 
-var income = job*clients
+var income = job*clients //calculating your annual income from web design/development
 
-alert("You generally get around $"+job+" for every client you work for. If you get "+clients+" a year that makes your annual income around $"+income+" a year.");
+alert("You generally get around $"+job+" for every client you work for. If you get "+clients+" a year that makes your annual income around $"+income+" a year."); //telling you how much money you make.
 
-alert("Good for you!");
+alert("Good for you!"); //Congradulating you for a job well done.
