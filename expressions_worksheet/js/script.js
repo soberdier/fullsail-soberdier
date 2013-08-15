dac33fb38db7709cae6dda03415ac8e744624755
soberdier <soberdier@gmail.com>
2013-08-15 17:06:30 -0400

    prompt and alert

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -1,23 +1,7 @@
-// Average shopping bill
+//Prompts and Alerts
 
-var groceries = [375, 250, 350, 274, 150]; //array of grocery bills for 5 weeks
-
-total = groceries[0] + groceries[1] + groceries[2] + groceries[3] + groceries[4] //total spent on groceries in 5 weeks
-
-average = total / 5; 
-
-console.log("You have spent a total of $"+total+" on groceries over five weeks. That is an average of $"+average+" per week.") //printing the result.
-
-//Discounts
-
-var original = 24.99; //item original price
-
-var discount = original*.10; //discounted price
-
-var product = "xboxone"; //item purchased
-
-taxless = original - discount //discounted item before tax
-
-tax = taxless*.70 //the sales tax
-taxed = original - discount + tax //the final price for this item.
-console.log("Your " +product+ " was originally $"+original+",but after a " +10+ "% discount, it is now $"+taxless+" without tax, and $"+taxed+" with tax.")
+var width = prompt("We are calculating the area of a rectangle. Please enter the width")
+var height = prompt("Please enter the height.")
+var area = width * height;
+//console.log(area);
+alert("The area of your rectangle is"+ area);
