31bb1bfa323a01eb2a0305d2cf2b9682ec0e68ab
soberdier <soberdier@gmail.com>
2013-08-15 11:05:48 -0400

    slice of pie part 1

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -9,4 +9,14 @@ dogAge = humanYears * dogYears; // calculating sparky's dog year age (result)
 console.log("Sparky is " +humanYears+ " years old in human years which is " +dogAge+ " years old in dog years.") // printing the calculation
 
 
-//
+//Slice of pie part1
+
+var slices = 10; //number of slices per pizza
+
+var people = 24; //number of people at the party
+
+var pizzas = 12; //number of pizzas ordered
+
+shares = pizzas * slices / people; 
+
+console.log("Each person ate " +shares+ " slices of pizza at the party.");
