668331d495c6afbef3dc7737495c566303fee1d4
soberdier <soberdier@gmail.com>
2013-08-15 10:51:25 -0400

    Dog Years complete

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -2,11 +2,11 @@
 
 var humanYears = 3; //declaring and defining sparky's age in human years
 
-const dogYears = 7; //declaring and defining 1 human year to dog years
+var dogYears = 7; //declaring and defining 1 human year to dog years
 
 dogAge = humanYears * dogYears; // calculating sparky's dog year age (result)
 
-console.log("Sparky is humanYears years old in human years which is dogAge years old in dog years.") // printing the calculation
+console.log("Sparky is " +humanYears+ " years old in human years which is " +dogAge+ " years old in dog years.") // printing the calculation
 
 
 //
