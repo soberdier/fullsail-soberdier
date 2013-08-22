96e6ea613c6d2fdaf3822733b69b3116f23f7ece
soberdier <soberdier@gmail.com>
2013-08-22 19:58:39 -0400

    conditionals personal (4)

--- conditionals/personal/js/script.js
+++ conditionals/personal/js/script.js
@@ -4,7 +4,7 @@
 
 var monster = 36; //price of a case of monster
 var payPerHour = prompt("How much do you get paid per hour?") //prompt asking how much you make an hour
-var hoursWorked = prompt("How many hours did you work this week?");
+var hoursWorked = prompt("How many hours did you work this week?"); //prompt asking how many hours you worked this week
 var bills = prompt("How much are your expenses this week?");
 var savings = payPerHour*hoursWorked/2
 if(payPerHour * hoursWorked - bills > monster)
