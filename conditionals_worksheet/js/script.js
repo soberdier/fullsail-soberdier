059ea660decc56b9725e6b5fad67f5abe89cae5f
soberdier <soberdier@gmail.com>
2013-08-22 17:38:51 -0400

    Temperature conversion

--- conditionals_worksheet/js/script.js
+++ conditionals_worksheet/js/script.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+// Conditionals worksheet
+// shane oberdier, 8-22, 
+//celsius to fahrenheit converter
+
+var temp = prompt("enter the temperature in celsius");
+var conversion = temp * 9 / 5 + 32 
+
+console.log("The temperature is "+conversion + " Degrees Fahrenheit.");
