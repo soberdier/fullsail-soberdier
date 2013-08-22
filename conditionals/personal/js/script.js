1a8a6ccdd799315198e70b4019703c0353be0ac5
soberdier <soberdier@gmail.com>
2013-08-22 19:57:03 -0400

    conditionals personal (2)

--- conditionals/personal/js/script.js
+++ conditionals/personal/js/script.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+// Shane Oberdier, 8-22, Conditionals/personal
+
+//Can you afford a case of Monster Energy Drinks this week?
+
+var monster = 36; //price of a case of monster
+var payPerHour = prompt("How much do you get paid per hour?")
+var hoursWorked = prompt("How many hours did you work this week?");
+var bills = prompt("How much are your expenses this week?");
+var savings = payPerHour*hoursWorked/2
+if(payPerHour * hoursWorked - bills > monster)
+	{
+		} 
