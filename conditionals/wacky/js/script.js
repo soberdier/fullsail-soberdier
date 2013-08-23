c2aee8d96918b974ea01c7fe53426de570aad210
soberdier <soberdier@gmail.com>
2013-08-22 21:12:04 -0400

    conditionals wacky(16)

--- conditionals/wacky/js/script.js
+++ conditionals/wacky/js/script.js
@@ -2,8 +2,8 @@
 
 // Let's try this again shall we?
 
-var intel = prompt("On a scale from 3 to 18, how intelligent are you?");
-var wis = prompt("On a scale from 3 to 18, how wise are you?");
-var perception = (parseInt(intel, 10) + parseInt(wis, 10))/2;
+var intel = prompt("On a scale from 3 to 18, how intelligent are you?"); //determining intelligence score
+var wis = prompt("On a scale from 3 to 18, how wise are you?"); //determining wisdom score
+var perception = (parseInt(intel, 10) + parseInt(wis, 10))/2; 
+// determining perception score
 
-console.log(perception)
