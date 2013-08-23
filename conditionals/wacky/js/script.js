8c03d5ed76e54958fe9a0e99981b7a1c21b689d3
soberdier <soberdier@gmail.com>
2013-08-22 21:09:28 -0400

    conditionals wacky(15)

---
+++ conditionals/wacky/js/script.js
@@ -0,0 +1,9 @@
+// Shane Oberdier, 8-22, conditionals wacky
+
+// Let's try this again shall we?
+
+var intel = prompt("On a scale from 3 to 18, how intelligent are you?");
+var wis = prompt("On a scale from 3 to 18, how wise are you?");
+var perception = (parseInt(intel, 10) + parseInt(wis, 10))/2;
+
+console.log(perception)
