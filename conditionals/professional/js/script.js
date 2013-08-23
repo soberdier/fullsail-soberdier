a6ddae4721b8f1c922f84acce57b32540f12d032
soberdier <soberdier@gmail.com>
2013-08-22 20:49:33 -0400

    conditionals professional(12)

--- conditionals/professional/js/script.js
+++ conditionals/professional/js/script.js
@@ -2,4 +2,11 @@
 
 // we will determine if a web design firm is willing to hire you
 
-var yearExp = prompt("How many years have you worked in your field?");
+var yearExp = prompt("How many years have you worked in your field?"); //asking how long you have done this job
+var companyResponse;
+
+// the company wants at least 5 years experience, let's see if you qualify
+
+companyResponse = (yearExp>5) ? "Congratulations you got the job!" : "I am sorry the position has been filled.";
+
+console.log(companyResponse);
