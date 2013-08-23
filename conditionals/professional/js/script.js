add10e2e7980d0ec413b2d90db98ae34bc43906a
soberdier <soberdier@gmail.com>
2013-08-22 20:44:41 -0400

    conditionals professional(11)

---
+++ conditionals/professional/js/script.js
@@ -0,0 +1,5 @@
+// Shane Oberdier, 8-22, conditionals professional
+
+// we will determine if a web design firm is willing to hire you
+
+var yearExp = prompt("How many years have you worked in your field?");
