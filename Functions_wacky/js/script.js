d35f718139450d8f7ea08abe0270e935e31f1f48
soberdier <soberdier@gmail.com>
2013-08-29 22:10:40 -0400

    wacky(13)

--- Functions_wacky/js/script.js
+++ Functions_wacky/js/script.js
@@ -3,3 +3,5 @@
 var cake = prompt("There is a cake sliced in 5 pieces on the table, How many do you take?"); //how gluttonous are you?
 
 var chuckNorris; //better watch out
+
+chuckNorris = (cake<=0)?"Chuck Norris is proud of your restraint and gives you Canada as a reward!":""
