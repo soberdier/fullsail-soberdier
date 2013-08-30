b48f7ca9cd0b84b556250ad4bc49f309a385526b
soberdier <soberdier@gmail.com>
2013-08-29 22:13:19 -0400

    wacky(16)

--- Functions_wacky/js/script.js
+++ Functions_wacky/js/script.js
@@ -5,3 +5,5 @@ var cake = prompt("There is a cake sliced in 5 pieces on the table, How many do 
 var chuckNorris; //better watch out
 
 chuckNorris = (cake<=0) ? "Chuck Norris is proud of your restraint and gives you Canada as a reward!":"That cake belonged to Chuck Norris, you have just been roundhouse kicked to death!"; //did you do well?
+
+console.log(chuckNorris)
