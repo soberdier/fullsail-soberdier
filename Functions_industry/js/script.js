88619b1f4e718129a1e12bd7f111e5a0b6be1b35
soberdier <soberdier@gmail.com>
2013-08-29 21:53:18 -0400

    functions industry(4)

--- Functions_industry/js/script.js
+++ Functions_industry/js/script.js
@@ -5,8 +5,11 @@ var deadLine;
 var overTime;
 function takeJob (pages,deadLine,overTime){
 	var pages = prompt("How many pages would you like the website to be?")
-	var deadLine = prompt("When do you want the site completed?")
-	var overTime = prompt("Are you willing to pay overtime if I have to work more than 40 hours?")
-	if (overtime = no){alert("I am sorry I can not take the job.")}
+	var deadLine = prompt("In how many days do you want the site completed?")
+	var overTime = prompt("How many hours will you allow before paying overtime?")
+	if (pages*8<=40|| overTime<=40){console.log("I will take the job.")}
+	else{alert("I'm sorry I can not take the job.")}
 	
 	}
+	takeJob();
+	
