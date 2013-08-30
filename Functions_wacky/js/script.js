66e71c87aa7f65275878669a5dedb0350ddd12c7
soberdier <soberdier@gmail.com>
2013-08-29 22:15:52 -0400

    wacky(20)

--- Functions_wacky/js/script.js
+++ Functions_wacky/js/script.js
@@ -1,9 +1,9 @@
 // Functions wacky
-
+//the chuck norris edition!
 var cake = prompt("There is a cake sliced in 5 pieces on the table, How many do you take?"); //how gluttonous are you?
 
 var chuckNorris; //better watch out
 
-chuckNorris = (cake<=0) ? "Chuck Norris is proud of your restraint and gives you Canada as a reward!":"That cake belonged to Chuck Norris, you have just been roundhouse kicked to death!"; //did you do well?
+chuckNorris = (cake<=0) ? "Chuck Norris is proud of your restraint and gives you Canada as a reward!":"That cake belonged to  Chuck Norris, you have just been roundhouse kicked to death!"; //did you do well?
 
 console.log(chuckNorris); //printing result
