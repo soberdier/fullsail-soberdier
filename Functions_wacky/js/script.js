9a8fc9d87fd64391190bd9eae27b197ba40b58e1
soberdier <soberdier@gmail.com>
2013-08-29 22:08:23 -0400

    wacky(11)

--- Functions_wacky/js/script.js
+++ Functions_wacky/js/script.js
@@ -1,5 +1,5 @@
 // Functions wacky
 
-var cake = prompt("There is a cake sliced in 5 pieces on the table, How many do you take?");
+var cake = prompt("There is a cake sliced in 5 pieces on the table, How many do you take?"); //how gluttonous are you?
 
-var cuckNorris;
+var chuckNorris;
