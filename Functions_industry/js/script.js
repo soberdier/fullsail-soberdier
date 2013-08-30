efbd1d781bc61973eba0d98823ded35e7f25965c
soberdier <soberdier@gmail.com>
2013-08-29 21:39:27 -0400

    functions industry(3)

---
+++ Functions_industry/js/script.js
@@ -0,0 +1,12 @@
+// Functions industry
+
+var pages;
+var deadLine;
+var overTime;
+function takeJob (pages,deadLine,overTime){
+	var pages = prompt("How many pages would you like the website to be?")
+	var deadLine = prompt("When do you want the site completed?")
+	var overTime = prompt("Are you willing to pay overtime if I have to work more than 40 hours?")
+	if (overtime = no){alert("I am sorry I can not take the job.")}
+	
+	}
