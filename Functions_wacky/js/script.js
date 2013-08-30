9b1d937c42ce71b1f717b96c1f59802866bf530d
soberdier <soberdier@gmail.com>
2013-08-29 22:14:01 -0400

    wacky(18)

--- Functions_wacky/js/script.js
+++ Functions_wacky/js/script.js
@@ -6,4 +6,4 @@ var chuckNorris; //better watch out
 
 chuckNorris = (cake<=0) ? "Chuck Norris is proud of your restraint and gives you Canada as a reward!":"That cake belonged to Chuck Norris, you have just been roundhouse kicked to death!"; //did you do well?
 
-console.log(chuckNorris);
+console.log(chuckNorris); //printing result
