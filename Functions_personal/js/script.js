0f5032571b38c34260f1f4452e2502258bbe12df
soberdier <soberdier@gmail.com>
2013-08-29 21:02:41 -0400

    anonymous function for personal(2)

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -1,5 +1,18 @@
 // Functions Personal
 
+var milliGrams;
+var smokeRate;
 var calcVapor = function(milliGrams,smokeRate){ //function calculating how long a bottle of fluid for an Ecigarette will last
-	
+	var milliGrams = prompt("How many milligrams of fluid do you have?");
+	var smokeRate = prompt("How many packs of cigarettes do you smoke a day?");
+	var Vapor = milliGrams * smokeRate;
+	return Vapor;
 	}
+	
+	
+	
+	
+	
+	var a = calcVapor(milliGrams,smokeRate) //defining funtion for output
+	
+	console.log("You will get "+a+" Days of use out of your fluid!")//printing the funtions results
